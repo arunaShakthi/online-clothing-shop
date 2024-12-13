46c776cebf0b6d3508e8dcd9e59699b3d64a44ee
arunaShakthi <arunashakthi002@gmail.com>
2024-12-13 21:51:02 +0530

    BestSeller items component

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -3,16 +3,17 @@ import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
 
-const ShopContextProvider = () => {
+const ShopContextProvider = (props)=> {
     
     const currency = '$';
     const delivery_fee = 10;
+    
     const value = {
         products, currency, delivery_fee
     }
 
     return (
-        <ShopContext.Provider>
+        <ShopContext.Provider value={value}>
             {props.children}
         </ShopContext.Provider>
     )
