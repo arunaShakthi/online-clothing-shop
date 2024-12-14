99ff3a3180dbaa488c9e09070a89602ec6d5c7aa
arunaShakthi <arunashakthi002@gmail.com>
2024-12-15 03:24:17 +0530

    product page functions

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { useSearchParams } from "react-router-dom";
 
@@ -8,13 +8,36 @@ const ShopContextProvider = (props)=> {
     
     const currency = '$';
     const delivery_fee = 10;
-
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
-    
+    const[cartItems, setCartItems] = useState({});
+
+    const addToCart = async (itemID,size) => {
+            let cartData = structuredClone(cartItems);
+
+            if (cartData[itemID]) {
+                if (cartData[itemID][size]) {
+                    cartData[itemID][size] += 1;
+                }
+                else {
+                    cartData[itemID][size] = 1;
+                } 
+            }
+            else {
+                cartData[itemID] = {};
+                cartData[itemID][size] = 1;
+            }
+            setCartItems(cartData);
+    }
+
+    useEffect(() =>{
+        console.log(cartItems);
+    },[cartItems])
+
     const value = {
         products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch
+        search, setSearch, showSearch, setShowSearch,
+        cartItems, addToCart
     }
 
     return (
