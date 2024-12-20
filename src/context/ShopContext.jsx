a93358985bff8fb50065bd12fdfae199dc866e85
arunaShakthi <arunashakthi002@gmail.com>
2024-12-20 18:08:55 +0530

    cart total func

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -2,7 +2,6 @@ import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { useSearchParams } from "react-router-dom";
 import { toast } from "react-toastify";
-import { cache } from "react";
 
 export const ShopContext = createContext();
 
@@ -54,15 +53,39 @@ const ShopContextProvider = (props)=> {
         return totalCount;
     }
 
-    useEffect(() =>{
-        console.log(cartItems);
-    },[cartItems])
+    const updateQuantity = async (itemId, size, quantity)  => {
+
+        let cartData = structuredClone(cartItems);
+
+        cartData[itemId][size] = quantity;
+
+        setCartItems(cartData);
+    }
+
+    const getCartAmount = () => {
+
+        let totalAmount = 0;
+        for(const items in cartItems) {
+            let itemInfo = products.find((product) => product._id === items);
+            for (const item in cartItems[items] > 0) {
+                try {
+                    if (cartItems[items][item] > 0) {
+                        totalAmount += itemInfo.price * cartItems[items][item]
+                    }
+                } catch (error) {
+
+                }
+            }
+        }
+        return totalAmount;
+    }
 
     const value = {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart,
-        getCartCount
+        getCartCount, updateQuantity,
+        getCartAmount
     }
 
     return (
